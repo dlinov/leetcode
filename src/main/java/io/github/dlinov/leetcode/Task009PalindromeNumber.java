bfaf5d575c4008268c70ebf07a90c9e5c7d6159e
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-03-11 17:58:27 +0400

    [java] Task 9, Palindrome Number

---
+++ src/main/java/io/github/dlinov/leetcode/Task009PalindromeNumber.java
@@ -0,0 +1,30 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task009PalindromeNumber {
+    class Solution {
+        public boolean isPalindrome(int x) {
+            if (x < 0) {
+                return false;
+            } else {
+                final int mult = 10;
+                int m = 1;
+                int digits = 1;
+                while (x / (m * mult) > 0 && digits < 10) {
+                    m *= mult;
+                    ++digits;
+                }
+                int tmp = x;
+                for (int i = 0; i < digits / 2; ++i) {
+                    if (tmp / m != tmp % mult) {
+                        return false;
+                    }
+                    tmp = (tmp % m) / mult;
+                    m /= mult * mult;
+                }
+                return true;
+            }
+        }
+    }
+}
