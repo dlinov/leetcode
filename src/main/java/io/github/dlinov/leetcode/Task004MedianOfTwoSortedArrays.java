53ee921ad0476bea0b6242e5b70a1a988c51e6ac
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-03-17 17:33:46 +0400

    [java] Task 10, Regular Expresssion Matching, 38/447 tests passed

---
+++ src/main/java/io/github/dlinov/leetcode/Task004MedianOfTwoSortedArrays.java
@@ -0,0 +1,14 @@
+package io.github.dlinov.leetcode;
+
+class Task004MedianOfTwoSortedArrays {
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            final int n1 = nums1.length;
+            final int n2 = nums2.length;
+            int i = n1 / 2;
+            int j = n2 / 2;
+            // while ()
+            return 0d;
+        }
+    }
+}
--- src/main/java/io/github/dlinov/leetcode/Task010RegularExpressionMatching.java
+++ src/main/java/io/github/dlinov/leetcode/Task010RegularExpressionMatching.java
@@ -3,22 +3,24 @@ package io.github.dlinov.leetcode;
 import java.util.*;
 
 class Task010RegularExpressionMatching {
-    private final static String inputTemplate = "Test if %s matches %s";
-    private final static String outputTemplate = "Actual: %b, expected: %b";
+    private final static String inputTemplate = "Test if %s matches %s...";
+    private final static String outputTemplate = "\tactual: %b, expected: %b";
     private static int overall = 0;
     private static int passed = 0;
 
     public static void main(String[] args) {
       final Solution s = new Solution();
-      // test(s, "aa", "a", false);
-      // test(s, "aa", "a*", true);
-      // test(s, "aab", "a*b", true);
-      // test(s, "aab", "a.b", true);
-      // test(s, "aab", "a.*b", true);
-      // test(s, "aab", "a.*c", false);
-      // test(s, "mississippi", "mis*is*p*.", false);
-      // test(s, "mississippi", "mis*is*ip*.", true);
-      test(s, "aaa", "a*a", true);
+    //   test(s, "aa", "a", false);
+    //   test(s, "aa", "a*", true);
+    //   test(s, "aab", "a*b", true);
+    //   test(s, "aab", "a.b", true);
+    //   test(s, "aab", "a.*b", true);
+    //   test(s, "aab", "a.*c", false);
+    //   test(s, "mississippi", "mis*is*p*.", false);
+    //   test(s, "mississippi", "mis*is*ip*.", true);
+    //   test(s, "aaa", "a*a", true);
+    //   test(s, "a", "ab*", true);
+      test(s, "bbbba", ".*a*a", true);
       System.out.println(String.format("\n=======\n%d tests passed out of %d", passed, overall));
     }
 
@@ -41,66 +43,56 @@ class Task010RegularExpressionMatching {
         }
     }
 
-    // TODO:
-    // "mississippi"
-    // "mis*is*ip*."
-    // actual: false
-    // expected: true
-
 static class Solution {
     public boolean isMatch(String s, String p) {
         int i = 0;
         int j = 0;
         boolean notFinished = true;
-        char lastToken = '\n'; // or other character guaranteed not to meet
         int lastStar = -1;
         int lastStarJ = -1;
         int lastStarReversedI = -1;
-        boolean lastChance = false;
         while (notFinished && i < s.length() && j < p.length()) {
             final char current = s.charAt(i);
-            char token = p.charAt(j);
-            boolean any = j + 1 < p.length() && p.charAt(j + 1) == '*';
+            final char token = p.charAt(j);
+            final boolean any = j + 1 < p.length() && p.charAt(j + 1) == '*';
             if (token == '.' || token == current) {
                 debug(String.format("'%c' matched token '%c'. i=%d, j=%d", current, token, i, j));
                 ++i;
                 if (any) {
                     lastStar = i;
                     lastStarJ = j;
-                    lastChance = true;
-                    debug(String.format("*. i=%d(%d), j=%d(%d)", i, s.length(), j, p.length()));
+                    debug(String.format("\t*. i=%d(%d), j=%d(%d)", i, s.length(), j, p.length()));
                     debug(String.format("\t%b", j + 2 < p.length() || i == s.length()));
                     if (j + 2 < p.length() || i == s.length()) {
                         j += 2;
                     }
                 } else {
                     ++j;
-                    lastChance = false;
                 }
             } else if (any) { // when * is exactly 0
                 debug(String.format("'%c' did not match token '%c'*. i=%d, j=%d", current, token, i, j));
                 j += 2;
-                lastChance = false;
             } else {
-                if (lastStar < 0 /*|| ???*/) {
+                if (lastStar < 0) {
                     debug(String.format("'%c' did not match token '%c'*. FORCE STOP. i=%d, j=%d", current, token, i, j));
                     notFinished = false;
-                    lastChance = false;
                 } else {
-                    i = lastStar + 1;
+                    i = lastStar;
                     j = lastStarJ;
-                    lastChance = false;
                     debug(String.format("'%c' did not match token '%c'*. REVERT TO i=%d, j=%d", current, token, i, j));
                 }
             }
             //specific case when not-eager matching ends too early
-            if (j == p.length() && i < s.length() && lastStar > -1 && lastStarReversedI != lastStar + 1) {
-                debug(String.format("'LAST CHANCE. REVERT TO i=%d, j=%d", i, j));
-                lastChance = false;
-                i = lastStar + 1;
+            if (j == p.length() && i < s.length() && lastStar > -1 && lastStarReversedI != lastStar) {
+                debug(String.format("LAST CHANCE. REVERT TO i=%d, j=%d", lastStar, lastStarJ));
+                i = lastStar;
                 lastStarReversedI = i;
                 j = lastStarJ;
             }
+        }
+        if (i >= s.length() && j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            debug(String.format("Trying to replace any * with exact 0; i=%d, j=%d, nextJ=%d", i, j, j + 2));
+            j += 2;
         }
         debug(String.format("i=%d, j=%d", i, j));
         return s.length() == i && p.length() == j;
