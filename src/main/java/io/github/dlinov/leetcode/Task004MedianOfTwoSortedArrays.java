6ddf924ad7e585f2d51d6dc2976efbeddb37c52a
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-03-21 15:36:32 +0400

    [java] [INCOMPLETE] Task 04, Media of Two Sorted Arrays

--- src/main/java/io/github/dlinov/leetcode/Task004MedianOfTwoSortedArrays.java
+++ src/main/java/io/github/dlinov/leetcode/Task004MedianOfTwoSortedArrays.java
@@ -5,9 +5,22 @@ class Task004MedianOfTwoSortedArrays {
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
             final int n1 = nums1.length;
             final int n2 = nums2.length;
-            int i = n1 / 2;
-            int j = n2 / 2;
-            // while ()
+            final int n3 = n1 + n2;
+            final boolean isEven1 = n1 % 2 == 0;
+            final boolean isEven2 = n2 % 2 == 0;
+            final boolean isEven3 = n3 % 2 == 0;
+            double m1 = 0;
+            double m2 = 0;
+            if (isEven1) {
+                m1 = (double) (nums1[n1 / 2] + nums2[n1 / 2]) / 2;
+            } else {
+                m1 = (double) nums1[n1 / 2];
+            }
+            if (isEven1) {
+                m1 = (double) (nums1[n1 / 2] + nums2[n1 / 2]) / 2;
+            } else {
+                m1 = (double) nums1[n1 / 2];
+            }
             return 0d;
         }
     }
