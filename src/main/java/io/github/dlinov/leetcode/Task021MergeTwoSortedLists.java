7b31a55fb05daeab50692f330ece188030f99b70
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-03-31 22:12:36 +0400

    [java] Task 21, Merge Two Sorted Lists

---
+++ src/main/java/io/github/dlinov/leetcode/Task021MergeTwoSortedLists.java
@@ -0,0 +1,43 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task021MergeTwoSortedLists {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            final ListNode fakeHead = new ListNode(0);
+            ListNode p = fakeHead;
+            ListNode p1 = l1;
+            ListNode p2 = l2;
+            while (p1 != null && p2 != null) {
+                if (p1.val < p2.val) {
+                    p.next = p1;
+                    p1 = p1.next;
+                } else {
+                    p.next = p2;
+                    p2 = p2.next;
+                }
+                p = p.next;
+            }
+            while (p1 != null) {
+                p.next = p1;
+                p1 = p1.next;
+                p = p.next;
+            }
+            while (p2 != null) {
+                p.next = p2;
+                p2 = p2.next;
+                p = p.next;
+            }
+            return fakeHead.next;
+        }
+    }
+}
