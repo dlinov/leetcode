a7793390a027c4c4cc155c48fc1f77812d01ee15
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-04-06 22:42:00 +0400

    [java] Task 26, Remove Duplicates from Sorted Array

---
+++ src/main/java/io/github/dlinov/leetcode/Task026RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task026RemoveDuplicatesFromSortedArray {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            final int length = nums.length;
+            if (length == 0) return 0;
+            int i = 0;
+            for (int k = 1; k < length; k++) {
+                if (nums[k] != nums[i]) {
+                    i++;
+                    nums[i] = nums[k];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
