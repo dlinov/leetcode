31988a79fb3204290fe8fc8d00dbc63758b30dee
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-04-07 03:39:30 +0400

    [java] Task 27, Remove Element

---
+++ src/main/java/io/github/dlinov/leetcode/Task027RemoveElement.java
@@ -0,0 +1,27 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task027RemoveElement {
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            final int length = nums.length;
+            if (length == 0) return 0;
+            int i = 0;
+            for (int j = 1; j < length; ++j) {
+                if (nums[i] == val && nums[j] != val) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                    ++i;
+                } else if (nums[i] != val) {
+                    ++i;
+                }
+            }
+            if (nums[i] != val) {
+                i++;
+            }
+            return i;
+        }
+    }
+}
