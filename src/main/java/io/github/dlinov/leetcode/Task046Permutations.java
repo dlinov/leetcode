ca2b2150f886494efb802723a85f6e65b2a5298e
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-04-29 18:44:43 +0400

    [java] Task 46, Permutations

---
+++ src/main/java/io/github/dlinov/leetcode/Task046Permutations.java
@@ -0,0 +1,48 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task046Permutations {
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            Arrays.sort(nums);
+            boolean isReversed = false;
+            List<List<Integer>> results = new ArrayList<>();
+            while (!isReversed) {
+                List<Integer> toAdd = new ArrayList<>(nums.length);
+                for (int i = 0; i< nums.length; ++i) {
+                    toAdd.add(nums[i]);
+                }
+                results.add(toAdd);
+                boolean readyForNext = false;
+                for (int i = nums.length - 2; i >= 0; --i) {
+                    if (nums[i] < nums[i + 1]) {
+                        for (int j = nums.length - 1; j > i; --j) {
+                            if (nums[j] > nums[i]) {
+                                // swap nums[j] > nums[i]
+                                int tmp = nums[j];
+                                nums[j] = nums[i];
+                                nums[i] = tmp;
+                                // reverse suffix
+                                i++;
+                                for (int k = nums.length - 1; k > i; --k, ++i) {
+                                    tmp = nums[k];
+                                    nums[k] = nums[i];
+                                    nums[i] = tmp;
+                                }
+                                readyForNext = true;
+                                break;
+                            }
+                        }
+                    }
+                    // break if done once
+                    if (readyForNext) {
+                        break;
+                    }
+                }
+                isReversed = !readyForNext;
+            }
+            return results;
+        }
+    }
+}
