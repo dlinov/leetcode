3dcd74420a62c37c30396471e23f5dafba750aa8
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-04-29 18:44:43 +0400

    [java] Task 46, Permutations

---
+++ src/test/scala/io/github/dlinov/leetcode/Task046Spec.scala
@@ -0,0 +1,22 @@
+package io.github.dlinov.leetcode
+
+import org.scalatest._
+
+import scala.collection.JavaConverters._
+
+class Task046Spec extends WordSpec with Matchers {
+    "Task 46, Permutations" must {
+        val task = new Task046Permutations()
+        val s = new task.Solution();
+        
+        "permute [1,2,3]" in {
+            val nums = Array(1, 2, 3)
+            s.permute(nums).asScala.toSet.size should be (6)
+        }
+        
+        "permute [1,5,8,10,4,7,6,2,3,9]" in {
+            val nums = Array(1, 5, 8, 10, 4, 7, 6, 2, 3, 9)
+            s.permute(nums).asScala.toSet.size should be (3628800)
+        }
+    }
+}
