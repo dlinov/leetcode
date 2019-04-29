ac5eb90a4e5b98a29da24c82cd05f0783bb6ee70
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-04-29 11:57:59 +0400

    [java] Task 43, Multiply Strings

---
+++ src/main/java/io/github/dlinov/leetcode/Task048RotateImage.java
@@ -0,0 +1,20 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task048RotateImage {
+    class Solution {
+        public void rotate(int[][] matrix) {
+            final int n = matrix.length;
+            for (int row = 0; row <= n / 2; ++row) {
+                for (int col = row; col < n - row - 1; ++col) {
+                    final int tmp = matrix[row][col];
+                    matrix[row][col] = matrix[n - 1 - col][row];
+                    matrix[n - 1 - col][row] = matrix[n - 1 - row][n - 1 - col];
+                    matrix[n - 1 - row][n - 1 - col] = matrix[col][n - 1 - row];
+                    matrix[col][n - 1 - row] = tmp;
+                }
+            }
+        }
+    }
+}
