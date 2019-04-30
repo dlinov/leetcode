87d3ba5e176c5eb4be6dd4dc795f912b76dd5aef
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-04-30 14:45:37 +0400

    [java] Task 47, Permutations II

---
+++ src/test/scala/io/github/dlinov/leetcode/Task047Spec.scala
@@ -0,0 +1,22 @@
+package io.github.dlinov.leetcode
+
+import org.scalatest._
+
+import scala.collection.JavaConverters._
+
+class Task047Spec extends WordSpec with Matchers {
+    "Task 47, Permutations II" must {
+        val task = new Task047PermutationsII()
+        val s = new task.Solution();
+        
+        "permute [1,2,1]" in {
+            val nums = Array(1, 2, 1)
+            s.permuteUnique(nums).asScala.toSet.size should be (3)
+        }
+        
+        "permute [1,5,8,5,4,7,6,2,3,9]" in {
+            val nums = Array(1, 5, 8, 5, 4, 7, 6, 2, 3, 9)
+            s.permuteUnique(nums).asScala.toSet.size should be (1814400)
+        }
+    }
+}
