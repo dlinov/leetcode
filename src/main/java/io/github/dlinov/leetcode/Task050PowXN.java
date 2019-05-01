5a8370683f5ad55e0df421d5c44d70d4516f7676
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-05-01 18:08:32 +0400

    [java] Task 50, Pow(x, n)

---
+++ src/main/java/io/github/dlinov/leetcode/Task050PowXN.java
@@ -0,0 +1,43 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task050PowXN {
+    class Solution {
+        public double myPow(double x, int n) {
+            if (n > 0) {
+                return myMultPow(x, n);
+            } else if (n < 0) {
+                return myDivPow(x, n);
+            } else {
+                return 1;
+            }
+        }
+
+        private double myMultPow(double x, int n) {
+            if (n == 1) {
+                return x;
+            } else {
+                double result = myMultPow(x, n / 2);
+                result *= result;
+                if (n % 2 == 1) {
+                    result *= x;
+                }
+                return result;
+            }
+        }
+
+        private double myDivPow(double x, int n) {
+            if (n == -1) {
+                return 1 / x;
+            } else {
+                double result = myDivPow(x, n / 2);
+                result *= result;
+                if (n % 2 == -1) {
+                    result /= x;
+                }
+                return result;
+            }
+        }
+    }
+}
