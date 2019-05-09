31b8cfe2573997e2602126db9d34f1e8e97e4a9c
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-05-09 16:14:45 +0400

    [java] Task 268, Missing Number

---
+++ src/main/java/io/github/dlinov/leetcode/Task268MissingNumber.java
@@ -0,0 +1,16 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+public class Task268MissingNumber {
+    class Solution {
+        public int missingNumber(int[] nums) {
+            int expectedSum = nums.length * (nums.length + 1) / 2;
+            int sum = nums[0];
+            for (int i = 1; i < nums.length; ++i) {
+                sum += nums[i];
+            }
+            return expectedSum - sum;
+        }
+    }
+}
