450d74b04ac0d32ae28c2012dd206ad50da9019b
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-05-09 14:30:33 +0400

    [java] Task 191, Number of 1 Bits

---
+++ src/main/java/io/github/dlinov/leetcode/Task191NumberOf1Bits.java
@@ -0,0 +1,27 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+public class Task191NumberOf1Bits {
+    class Solution {
+        // you need to treat n as an unsigned value
+        public int hammingWeight(int n) {
+            int result = 0;
+            final boolean isNeg = n < 0;
+            int x = n;
+            if (isNeg) {
+                x = ~x;
+            }
+            while (x > 0) {
+                if (x % 2 != 0) {
+                    ++result;
+                }
+                x /= 2;
+            }
+            if (isNeg) {
+                result = 32 - result;
+            }
+            return result;
+        }
+    }
+}
