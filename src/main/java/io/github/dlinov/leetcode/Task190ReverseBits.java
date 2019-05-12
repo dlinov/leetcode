b9696bbaea6924970cecdc05c7c9c655109d89d2
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-05-12 12:34:54 +0400

    [java] Task 190, Reverse Bits

---
+++ src/main/java/io/github/dlinov/leetcode/Task190ReverseBits.java
@@ -0,0 +1,19 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+public class Task190ReverseBits {
+    class Solution {
+        public int reverseBits(int n) {
+            int result = 0;
+            int src = n;
+            while (src != 0) {
+                int lastBit = src & 1;
+                result <<= 1;
+                result |= lastBit;
+                src >>= 1;
+            }
+            return result;
+        }
+    }
+}
