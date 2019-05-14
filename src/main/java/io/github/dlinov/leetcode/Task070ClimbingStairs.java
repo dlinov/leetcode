1250d682d73a6b6439b56e30e37d60993a8deb3d
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-05-14 10:51:36 +0400

    [java] Task 70, Climbing Stairs

---
+++ src/main/java/io/github/dlinov/leetcode/Task070ClimbingStairs.java
@@ -0,0 +1,21 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+public class Task070ClimbingStairs {
+    class Solution {
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            int p1 = 1;
+            int p2 = 2;
+            for (int i = 2; i < n; ++i)  {
+                int next = p1 + p2;
+                p1 = p2;
+                p2 = next;
+            }
+            return p2;
+        }
+    }
+}
