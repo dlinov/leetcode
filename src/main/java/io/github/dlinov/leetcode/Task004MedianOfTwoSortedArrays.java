62682913b37a05496afe8e53a7c24a8078992f3a
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-10-30 02:14:19 +0300

    [java] Task 4, Median of Two Sorted Arrays

--- src/main/java/io/github/dlinov/leetcode/Task004MedianOfTwoSortedArrays.java
+++ src/main/java/io/github/dlinov/leetcode/Task004MedianOfTwoSortedArrays.java
@@ -3,25 +3,40 @@ package io.github.dlinov.leetcode;
 class Task004MedianOfTwoSortedArrays {
     class Solution {
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            final int n1 = nums1.length;
-            final int n2 = nums2.length;
-            final int n3 = n1 + n2;
-            final boolean isEven1 = n1 % 2 == 0;
-            final boolean isEven2 = n2 % 2 == 0;
-            final boolean isEven3 = n3 % 2 == 0;
-            double m1 = 0;
-            double m2 = 0;
-            if (isEven1) {
-                m1 = (double) (nums1[n1 / 2] + nums2[n1 / 2]) / 2;
+            if (nums1.length <= nums2.length) {
+                return median(nums1, nums2);
             } else {
-                m1 = (double) nums1[n1 / 2];
+                return median(nums2, nums1);
             }
-            if (isEven1) {
-                m1 = (double) (nums1[n1 / 2] + nums2[n1 / 2]) / 2;
-            } else {
-                m1 = (double) nums1[n1 / 2];
+        }
+
+        double median(int[] arr1, int[] arr2) {
+            int n = arr1.length;
+            int m = arr2.length;
+            int low = 0;
+            int high = n;
+            double result = Double.NEGATIVE_INFINITY;
+            while (low <= high) {
+                int i = (low + high) / 2;
+                int j = (n + m + 1) / 2 - i;
+                int maxLeftX = i > 0 ? arr1[i - 1] : Integer.MIN_VALUE;
+                int minRightX = i == n ? Integer.MAX_VALUE : arr1[i];
+                int maxLeftY = j > 0 ? arr2[j - 1] : Integer.MIN_VALUE;
+                int minRightY = j == m ? Integer.MAX_VALUE : arr2[j];
+                if (maxLeftX <= minRightY && minRightX >= maxLeftY) {
+                    if ((n + m) % 2 == 0) {
+                        result = (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+                    } else {
+                        result = Math.max(maxLeftX, maxLeftY);
+                    }
+                    break;
+                } else if (maxLeftX > minRightY) {
+                    high = i - 1;
+                } else {
+                    low = i + 1;
+                }   
             }
-            return 0d;
+            return result;
         }
     }
 }
