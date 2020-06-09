b8cabfb688ccefd50c81c1242987cf1cdcdd5f23
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-06-09 13:09:26 +0300

    [java] Task 392, Is Subsequence

---
+++ src/main/java/io/github/dlinov/leetcode/Task392IsSubsequence.java
@@ -0,0 +1,19 @@
+package io.github.dlinov.leetcode;
+
+public class Task392IsSubsequence {
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+            if (s.isEmpty()) return true;
+            int j = 0;
+            for (int i = 0; i < t.length(); ++i) {
+                if (t.charAt(i) == s.charAt(j)) {
+                    ++j;
+                    if (j == s.length()) {
+                        break;
+                    }
+                }
+            }
+            return j == s.length();
+        }
+    }
+}
