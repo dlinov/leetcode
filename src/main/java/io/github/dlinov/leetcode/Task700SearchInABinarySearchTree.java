2280ffc4c5a3ab6d23d204cac0db3105f9532d76
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-06-15 23:49:58 +0300

    [java] Task 700, Search in a Binary Search Tree

---
+++ src/main/java/io/github/dlinov/leetcode/Task700SearchInABinarySearchTree.java
@@ -0,0 +1,25 @@
+package io.github.dlinov.leetcode;
+
+public class Task700SearchInABinarySearchTree {
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if (root == null) return null;
+            if (root.val == val) return root;
+            if (root.val > val) return searchBST(root.left, val);
+            return searchBST(root.right, val);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
