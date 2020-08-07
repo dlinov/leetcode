1ece787939bfc83e36af2a2b9d3d2321adeaf0f4
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-08-08 02:04:44 +0300

    [java] Task 1450, Number of Students Doing Homework at a Given Time

---
+++ src/main/java/io/github/dlinov/leetcode1k/Task1450NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,15 @@
+package io.github.dlinov.leetcode1k;
+
+public class Task1450NumberOfStudentsDoingHomeworkAtAGivenTime {
+    class Solution {
+        public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+            int result = 0;
+            for (int i = 0; i < startTime.length; ++i) {
+                if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                    ++result;
+                }
+            }
+            return result;
+        }
+    }
+}
