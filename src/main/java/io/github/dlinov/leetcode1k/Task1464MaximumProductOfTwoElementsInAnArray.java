319c5346d88012477dc20b6732423209ca5a7e22
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-08-08 02:09:37 +0300

    [java] Task 1464, Maximum Product of Two Elements in an Array

---
+++ src/main/java/io/github/dlinov/leetcode1k/Task1464MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,16 @@
+package io.github.dlinov.leetcode1k;
+
+public class Task1464MaximumProductOfTwoElementsInAnArray {
+    class Solution {
+        public int maxProduct(int[] nums) {
+            int max = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                for (int j = 0; j < nums.length; ++j) {
+                    if (i == j) continue;
+                    max = Math.max(max, (nums[i] - 1) * (nums[j] - 1));
+                }
+            }
+            return max;
+        }
+    }
+}
