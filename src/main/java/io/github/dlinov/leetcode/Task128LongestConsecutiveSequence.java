5ce03ea61b30f31f6c937f205bfdabb6804f4b8e
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-09-05 12:44:34 +0300

    Fix formatting

--- src/main/java/io/github/dlinov/leetcode/Task128LongestConsecutiveSequence.java
+++ src/main/java/io/github/dlinov/leetcode/Task128LongestConsecutiveSequence.java
@@ -5,26 +5,26 @@ import java.util.*;
 public class Task128LongestConsecutiveSequence {
     class Solution {
         public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-        final TreeSet<Integer> numSet = new TreeSet<>();
-        int min = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] < min) min = nums[i];
-            numSet.add(nums[i]);
-        }
-        int maxResult = 1, currResult = 1;
-        Integer curr = min;
-        while (numSet.higher(curr) != null) {
-            if (numSet.contains(curr + 1)) {
-                ++currResult;
-                ++curr;
-            } else {
-                maxResult = Math.max(maxResult, currResult);
-                currResult = 1;
-                curr = numSet.higher(curr);
+            if (nums.length == 0) return 0;
+            final TreeSet<Integer> numSet = new TreeSet<>();
+            int min = Integer.MIN_VALUE;
+            for (int i = 0; i < nums.length; ++i) {
+                if (nums[i] < min) min = nums[i];
+                numSet.add(nums[i]);
             }
-        }
-        return Math.max(maxResult, currResult);
+            int maxResult = 1, currResult = 1;
+            Integer curr = min;
+            while (numSet.higher(curr) != null) {
+                if (numSet.contains(curr + 1)) {
+                    ++currResult;
+                    ++curr;
+                } else {
+                    maxResult = Math.max(maxResult, currResult);
+                    currResult = 1;
+                    curr = numSet.higher(curr);
+                }
+            }
+            return Math.max(maxResult, currResult);
         }
     }
 }
