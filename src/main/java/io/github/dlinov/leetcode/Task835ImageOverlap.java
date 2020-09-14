b8be38843a82008914bde7710838d320481829d3
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-09-15 02:19:48 +0300

    [java] Task 835, Image Overlap

---
+++ src/main/java/io/github/dlinov/leetcode/Task835ImageOverlap.java
@@ -0,0 +1,25 @@
+package io.github.dlinov.leetcode;
+
+public class Task835ImageOverlap {
+    class Solution {
+        public int largestOverlap(int[][] A, int[][] B) {
+            int largestOverlap = 0;
+            for (int row = -A.length + 1; row < A.length; row++)
+                for (int col = -A[0].length + 1; col < A[0].length; col++)
+                    largestOverlap = Math.max(largestOverlap, overlapping(A, B, row, col));
+            return largestOverlap;
+        }
+    
+        public int overlapping(int[][] A, int[][] B, int rowOffset, int colOffset) {
+            int overlapOnes = 0;
+            for (int row = 0; row < A.length; row++) {
+                for (int col = 0; col < A[0].length; col++) {
+                    if ((row + rowOffset < 0 || row + rowOffset >= A.length) || (col + colOffset < 0 || col + colOffset >= A.length) || (A[row][col] + B[row + rowOffset][col + colOffset] != 2))
+                        continue;
+                    overlapOnes++;
+                }
+            }
+            return overlapOnes;
+        }
+    }    
+}
