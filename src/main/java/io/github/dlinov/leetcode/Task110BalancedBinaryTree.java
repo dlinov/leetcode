b8fdc5e47700b9b5a9bf02c502c93c3c2d61d73e
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2020-12-23 02:51:47 +0300

    [java], Task 110, Balanced Binary Tree

---
+++ src/main/java/io/github/dlinov/leetcode/Task110BalancedBinaryTree.java
@@ -0,0 +1,36 @@
+package io.github.dlinov.leetcode;
+
+public class Task110BalancedBinaryTree {
+    class Solution {
+    private boolean result = true;
+
+    public boolean isBalanced(TreeNode root) {
+        maxDepth(root);
+        return result;
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        if (Math.abs(l - r) > 1) result = false;
+        return 1 + Math.max(l, r);
+    }
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
