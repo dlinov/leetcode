79dcc7baece086a8ebcce55f43942c0294ec799d
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2021-02-07 17:53:31 +0300

    [java] Task 594, Longest Harmonious Subsequence

---
+++ src/main/java/io/github/dlinov/leetcode/Task594LongestHarmoniousSubsequence.java
@@ -0,0 +1,18 @@
+package io.github.dlinov.leetcode;
+
+public class Task594LongestHarmoniousSubsequence {
+    class Solution {
+        public int findLHS(int[] nums) {
+            final Map<Integer, Integer> map = new HashMap<>();
+            int res = 0;
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            for (int key : map.keySet()) {
+                if (map.containsKey(key + 1))
+                    res = Math.max(res, map.get(key) + map.get(key + 1));
+            }
+            return res;
+        }
+    }
+}
