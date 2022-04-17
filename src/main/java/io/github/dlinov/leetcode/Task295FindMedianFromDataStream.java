939af06775075f114813f1fbdb7994faabd3c8dc
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2022-04-17 15:13:00 +0300

    [java] Task 295, Find Median from Data Stream

---
+++ src/main/java/io/github/dlinov/leetcode/Task295FindMedianFromDataStream.java
@@ -0,0 +1,29 @@
+package io.github.dlinov.leetcode;
+
+import java.util.*;
+
+class Task295FindMedianFromDataStream {
+    class Solution {
+        private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+        private PriorityQueue<Integer> large = new PriorityQueue<>();
+        private boolean even = true;
+
+        public double findMedian() {
+            if (even)
+                return (small.peek() + large.peek()) / 2.0;
+            else
+                return small.peek();
+        }
+
+        public void addNum(int num) {
+            if (even) {
+                large.offer(num);
+                small.offer(large.poll());
+            } else {
+                small.offer(num);
+                large.offer(small.poll());
+            }
+            even = !even;
+        }
+    }
+}
